b72d67d5fffc85cd87cd2d58e00168ce1691b1b9
tarex7 <tarex7@gmail.com>
2022-06-27 16:16:27 +0200

    MILESTONE 2 OK

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 //Buttons
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+const gallery = document.querySelector(".gallery");
+const thumbnails = document.querySelector(".thumbnails");
+
+//Active index
+let currentActiveIndex = 0;
+
+//Array immagini
+const imgSrc = [
+  "./img/01.jpg",
+  "./img/02.jpg",
+  "./img/03.jpg",
+  "./img/04.jpg",
+  "./img/05.jpg",
+];
+
+//Crea HTML immagini
+let image = "";
+for (let i = 0; i < imgSrc.length; i++) {
+  image += `<img src=${imgSrc[i]}  alt = "landscape-${i + 1}"/>`;
+}
+
+//Aggiunge immagini nell HTML
+gallery.innerHTML = image;
+thumbnails.innerHTML = image;
+
+//Prende tutte le immagini nella gallery
+const images = document.querySelectorAll(".gallery img");
+
+//Mostra imagine
+images[currentActiveIndex].classList.add("d-block");
+
